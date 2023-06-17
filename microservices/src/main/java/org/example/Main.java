1e391c21a608197c9789d5e3c67b33b5c8da5f94
Oluwatomi <ooloye@ecobank.com>
2023-06-17 23:57:37 +0100

    Feat: Implemented the MongoDb Connection from springboot added test data to the DB

--- microservices/src/main/java/org/example/Main.java
+++ microservices/src/main/java/org/example/Main.java
@@ -1,18 +1,73 @@
 package org.example;
 
+import org.example.model.Address;
+import org.example.model.Gender;
+import org.example.model.Student;
+import org.example.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.*;
 
 
-//@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
-//@SpringBootApplication(exclude={MongoAutoConfiguration.class})
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         SpringApplication.run(Main.class);
+    }
+
+    @Bean
+    CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate){
+        return  args -> {
+            Address address = new Address(
+                    "Canada",
+                    "4258033",
+                    "New Brunswick"
+            );
+            String email = "davidcode@gmail.com";
+            Student student = new Student(
+                    "Oluwatomi",
+                    "Oloye",
+                    email,
+                    Gender.MALE,
+                    address,
+                    Arrays.asList("Test Coding", "Maths"),
+                    BigDecimal.TEN,
+                    LocalDateTime.now());
+
+            repository.findStudentByEmail(email).ifPresent(student1 -> {
+                System.out.println(" ::: Student Already Exist::::" + student);
+            });
+
+
+
+
+        };
+    }
+
+
+    private void UsingMongoeTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, String email, Student student){
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").is(email));
+        List<Student> students = mongoTemplate.find(query, Student.class);
+
+        if(students.size() > 1){
+            throw new IllegalStateException(" :::::: found many students with email :::: " + student.getEmail());
+        }
+        if(students.isEmpty()){
+            System.out.println("::::: Inserting Students ::: " + student);
+            repository.insert(student);
+        }
+        else {
+            System.out.println(" ::: Student Already Exist::::" + student);
+        }
     }
 }
---
+++ microservices/src/main/java/org/example/model/Student.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Document
+@Data
+public class Student {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+//    @Indexed(unique = true)
+    private String email;
+    private Gender gender;
+    private Address address;
+    private List<String> favouriteSubjects;
+    private BigDecimal totalSpentInBooks;
+    private LocalDateTime create;
+
+
+    public Student(String firstName, String lastName, String email, Gender gender, Address address, List<String> favouriteSubjects, BigDecimal totalSpentInBooks, LocalDateTime create) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.address = address;
+        this.favouriteSubjects = favouriteSubjects;
+        this.totalSpentInBooks = totalSpentInBooks;
+        this.create = create;
+    }
+}
