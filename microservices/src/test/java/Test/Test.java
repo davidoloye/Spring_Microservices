09bd3f4c7f20171d6ebf55845f0ba1606cb29ea6
Oluwatomi <ooloye@ecobank.com>
2023-06-19 21:25:11 +0100

    Feat: Implementing an Integrated test class for testing the endpoints

--- microservices/src/test/java/Test/Test.java
+++ microservices/src/test/java/Test/Test.java
@@ -1,16 +1,33 @@
 package Test;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@SpringBootTest
+@Testcontainers
+@RequiredArgsConstructor
 public class Test {
 
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongodb:3.8");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void contextLoads(){
+
+    }
 
 
-
-//    public void whenPropertiesConfig_thenInsertSucceeds() {
-//        SpringApplicationBuilder app = new SpringApplicationBuilder(Test.class)
-//        app.run();
-//
-//        assertInsertSucceeds(app.context());
-//    }
 }
